b94ae4ef1bd49f61bdb9bf316a05c4b5da996b5f
soberanoJr <69157717+soberanoJr@users.noreply.github.com>
2022-05-06 19:55:28 +0000

    snake

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 
 screen = Screen()
 screen.setup(width=1.0, height=1.0)
 screen.bgcolor("black")
-screen.title( 
-    "===+" * 5 +
-    "===:> snake"
-)
+screen.title("===+" * 5 +"===:> snake")
+coordinates = [(0, 0), (-20, 0), (-40, 0)]
+
+for coordinate in coordinates:
+    snake = Turtle(shape="square")
+    snake.color("white")
+    snake.goto(coordinate)
