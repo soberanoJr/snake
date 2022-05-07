e39f45948f5de90f45b14cb868eec281910f025c
soberanoJr <69157717+soberanoJr@users.noreply.github.com>
2022-05-07 13:14:26 +0000

    control

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ import time
 
 
 screen = Screen()
+snake = Snake()
+play = True
+
 screen.setup(width=1.0, height=1.0)
 screen.bgcolor("black")
 screen.title("===+" * 5 + "===:> snake")
 screen.tracer(0)
-snake = Snake()
-play = True
+screen.listen()
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
 
 
 while play:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
--- snake.py
+++ snake.py
@@ -3,12 +3,18 @@ from turtle import Turtle
 
 COORDINATES = [(0, 0), (-20, 0), (-40, 0)]
 DISTANCE = 20
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
 
 class Snake:
 
     def __init__(self):
-        self.tail = []
+        self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for coordinate in COORDINATES:
@@ -16,12 +22,27 @@ class Snake:
             snake.color("white")
             snake.penup()
             snake.goto(coordinate)
-            self.tail.append(snake)
-
+            self.segments.append(snake)
 
     def move(self):
-        for _ in range(len(self.tail) -1, 0, -1):
-            x = self.tail[_ - 1].xcor()
-            y = self.tail[_ - 1].ycor()
-            self.tail[_].goto(x, y)
-        self.tail[0].forward(DISTANCE)
+        for _ in range(len(self.segments) -1, 0, -1):
+            x = self.segments[_ - 1].xcor()
+            y = self.segments[_ - 1].ycor()
+            self.segments[_].goto(x, y)
+        self.head.forward(DISTANCE)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)    
+    
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+        
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
