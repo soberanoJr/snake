b3292cf94d14d2bb99eb3f1d46cfd00f4530cf47
soberanoJr <tonysoberano@gmail.com>
2022-05-10 15:08:20 -0300

    food

--- food.py
+++ food.py
@@ -5,18 +5,17 @@ import random
 class Food(Turtle):
 
     def __init__(self):
+
         super().__init__()
         self.shape("circle")
         self.penup()
-        self.shapesize(
-            stretch_len=0.75, 
-            stretch_wid=0.75
-        )
+        self.shapesize(stretch_len=0.75, stretch_wid=0.75)
         self.color("yellow")
         self.speed("fastest")
         self.refresh()
 
     def refresh(self):
+
         x = random.uniform(-330, 330)
         y = random.uniform(-230, 230)
         self.goto(x, y)
