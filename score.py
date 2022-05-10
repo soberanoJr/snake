6ac09f77276e9b0463818eb195a9e35bd1617cd1
soberanoJr <tonysoberano@gmail.com>
2022-05-10 15:19:01 -0300

    score

--- score.py
+++ score.py
@@ -8,29 +8,26 @@ FONT = ("Arial", 64, "normal")
 class Score(Turtle):
 
     def __init__(self):
+
         super().__init__()
         self.score = 0
         self.color("yellow")
         self.penup()
-        self.goto(0,0)
+        self.goto(0, 0)
         self.hideturtle()
         self.update()
 
     def update(self):
-        self.write(self.score, 
-                   align=ALIGNMENT, 
-                   font=FONT
-        )
-    
+
+        self.write(self.score, align=ALIGNMENT, font=FONT)
+
     def count(self):
+
         self.score += 1
         self.clear()
         self.update()
 
-
     def game_over(self):
+
         self.clear()
-        self.write("GAME OVER", 
-                   align=ALIGNMENT, 
-                   font=FONT
-        )
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
